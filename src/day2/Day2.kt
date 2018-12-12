6cb618d663857785f91c170cfa09350bd23fa6ad
Otto Kaaij <o.k.n.kaaij@student.tudelft.nl>
2018-12-12 16:54:49 +0100

    Add solution to part1 of day2

--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -1,2 +1,43 @@
 package day2
 
+import java.io.File
+import java.util.*
+import kotlin.collections.HashMap
+
+fun main(args: Array<String>) {
+    println("part1() = ${part1()}")
+}
+
+fun part1(): Int {
+    val sc = Scanner(File("resource/day2"))
+    var twice = 0
+    var thrice = 0
+
+    while (sc.hasNext()) {
+        val word = sc.nextLine()
+        val map = HashMap<Char, Int>()
+
+        for (char in word) {
+            map.putIfAbsent(char, 0)
+            map[char] = map.getValue(char) + 1
+
+        }
+        if (map.contains {i -> i == 2}) {
+            twice ++
+        }
+        if (map.contains {i -> i == 3}) {
+            thrice ++
+        }
+    }
+    return twice * thrice
+}
+
+
+fun HashMap<Char, Int>.contains(function: (Int) -> Boolean) : Boolean {
+    for ( thing in this ) {
+        if (function(thing.value)) {
+            return true
+        }
+    }
+    return false
+}
