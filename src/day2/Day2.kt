d6b12f1c6fb10dbef173c453ff4b2e66044fffc4
Otto Kaaij <o.k.n.kaaij@student.tudelft.nl>
2018-12-13 14:52:53 +0100

    Finish day2

--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -2,10 +2,13 @@ package day2
 
 import java.io.File
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 fun main(args: Array<String>) {
     println("part1() = ${part1()}")
+    println("part2() = ${part2()}")
+
 }
 
 fun part1(): Int {
@@ -32,6 +35,42 @@ fun part1(): Int {
     return twice * thrice
 }
 
+fun part2(): String {
+
+    val words = ArrayList<String>()
+    val sc = Scanner(File("resource/day2"))
+
+    while (sc.hasNext()) {
+        words.add(sc.nextLine())
+    }
+
+    words.forEach {
+        val index = compareWords(it, words)
+        if (index != -1) {
+            return it.removeRange(index, index + 1)
+        }
+    }
+    return ""
+}
+
+fun compareWords(word: String, words: ArrayList<String>) : Int {
+    words.forEach {cmpWord ->
+        var firstMatch = -1
+        var secondMatch = -1
+        word.forEachIndexed {index, c ->
+            if (cmpWord[index] != c) {
+                if (firstMatch != -1) {
+                    secondMatch = index
+                }
+                firstMatch = index
+            }
+        }
+        if (firstMatch != -1 && secondMatch == -1) {
+            return firstMatch
+        }
+    }
+    return -1
+}
 
 fun HashMap<Char, Int>.contains(function: (Int) -> Boolean) : Boolean {
     for ( thing in this ) {
